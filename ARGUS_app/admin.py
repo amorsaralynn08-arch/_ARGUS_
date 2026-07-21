3633d49985d9b6f0a1f35660452b58992ea07971
amor <amorsaralynn@gmail.com>
2026-07-21 18:37:59 +0300

    register vehicle and SensorReading models in admin.py

--- ARGUS_app/admin.py
+++ ARGUS_app/admin.py
@@ -5,3 +5,5 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(User, UserAdmin)
+admin.site.register(Vehicle)
+admin.site.register(SensorReading)
