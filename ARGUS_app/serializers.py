3796ab8d88c7e80d89034f646fb2f97bc3f4fc03
amor <amorsaralynn@gmail.com>
2026-07-22 21:57:31 +0300

    add an Alert view using viewsets

--- ARGUS_app/serializers.py
+++ ARGUS_app/serializers.py
@@ -5,3 +5,8 @@ class SensorReadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = SensorReading
         fields = "__all__"
+
+class AlertSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alert
+        fields = "__all__"
--- ARGUS_app/views.py
+++ ARGUS_app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import SensorReading
+from rest_framework import viewsets
+from .models import *
 from .serializers import SensorReadingSerializer
+from .serializers import AlertSerializer
+
 
 # Create your views here.
 class SensorReadingListCreateView(generics.ListCreateAPIView):
     queryset = SensorReading.objects.all()
     serializer_class = SensorReadingSerializer  
+
+class AlertViewSet(viewsets.ModelViewSet):
+    queryset = Alert.objects.all()
+    serializer_class = AlertSerializer
