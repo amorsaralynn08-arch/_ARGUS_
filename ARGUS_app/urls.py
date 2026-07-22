dc6a50a62adf6c2d255299f551819f5b1f308901
amor <amorsaralynn@gmail.com>
2026-07-22 23:07:37 +0300

    Add and use router to configure urls for the Alert viewset

--- ARGUS_app/urls.py
+++ ARGUS_app/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import SensorReadingListCreateView
+from django.urls import path , include
+from .views import SensorReadingListCreateView,AlertViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("alerts" ,AlertViewSet)
 
 urlpatterns = [
     path("sensor-readings/",SensorReadingListCreateView.as_view(),name="sensor-readings"),
+    path("", include(router.urls)),
 ]
