d04ac64eb3d9e02bc71d745c0204933e45b79214
amor <amorsaralynn@gmail.com>
2026-07-23 21:01:33 +0300

    edit urls to avoid redundancy

--- ARGUS_app/urls.py
+++ ARGUS_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path , include
 from .views import SensorReadingListCreateView,AlertViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView , TokenRefreshView)
+
 
 router = DefaultRouter()
 router.register("alerts" ,AlertViewSet)
@@ -8,4 +10,6 @@ router.register("alerts" ,AlertViewSet)
 urlpatterns = [
     path("sensor-readings/",SensorReadingListCreateView.as_view(),name="sensor-readings"),
     path("", include(router.urls)),
+    path("token/",TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
