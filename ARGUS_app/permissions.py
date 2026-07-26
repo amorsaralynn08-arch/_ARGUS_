333066d7d195789886c064cefd780ea21e422461
amor <amorsaralynn@gmail.com>
2026-07-26 23:55:19 +0300

    customise permissions to roles

--- ARGUS_app/permissions.py
+++ ARGUS_app/permissions.py
@@ -1,8 +1,28 @@
 from rest_framework.permissions import BasePermission
 from .models import User
 
-class IsAdminOrFleetManager(BasePermission):
+class CanViewSensorReadings(BasePermission):
+    ALLOWED_ROLES=(
+        User.Role.ADMIN,
+        User.Role.FLEET_MANAGER,
+        User.Role.DRIVER,
+        User.Role.MECHANIC,
+    )
+
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated and request.user.role in [User.ADMIN , User.FLEET_MANAGER]
+            request.user.is_authenticated and request.user.role in self.ALLOWED_ROLES
+        )
+
+class CanManageAlerts(BasePermission):
+    ALLOWED_ROLES = (
+        User.Role.ADMIN,
+        User.Role.FLEET_MANAGER,
+
+    )
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and 
+            request.user.role in self.ALLOWED_ROLES
         )
