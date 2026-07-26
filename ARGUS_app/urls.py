db1e7e2d87e6c5ceb900c44139bdef96d7577911
amor <amorsaralynn@gmail.com>
2026-07-26 22:53:33 +0300

    update urls from generic to custom viewset

--- ARGUS_app/urls.py
+++ ARGUS_app/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path , include
-from .views import SensorReadingListCreateView,AlertViewSet
+from .views import SensorReadingView,AlertViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView , TokenRefreshView)
 
 
 router = DefaultRouter()
-router.register(r"alerts" ,AlertViewSet,basename="alert",)
+router.register(r"alerts" , AlertViewSet , basename="alert",)
+router.register(r"sensor-readings",SensorReadingView,basename="sensor-reading")
+
 
 urlpatterns = [
-    path("sensor-readings/",SensorReadingListCreateView.as_view(),name="sensor-readings"),
+    
     path("", include(router.urls)),
     path("token/",TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
 ]
