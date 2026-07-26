a21301b214950d214c264a88dd887ee2dba39adf
amor <amorsaralynn@gmail.com>
2026-07-27 01:05:20 +0300

    cutstomize django-admins interface for companies

--- ARGUS_app/admin.py
+++ ARGUS_app/admin.py
@@ -3,7 +3,14 @@ from .models import *
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(Company)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name","email","phone_number","is_active","created_at")
+    search_fields = ("name", "email")
+    list_filter = ("is_active","created_at")
+    ordering = ("name")
+    readonly_fields = ("created_at")
+    
 admin.site.register(User, UserAdmin)
 admin.site.register(Vehicle)
 admin.site.register(SensorReading)
