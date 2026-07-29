02762680e715f1ec0618aa956e255d050d04462e
amor <amorsaralynn@gmail.com>
2026-07-29 10:29:06 +0300

    create email helper function

---
+++ ARGUS_app/utils.py
@@ -0,0 +1,13 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+
+def send_test_email(recipient_email):
+    subject = "Welcome to ARGUS"
+    text_content = "This is a test email from ARGUS"
+    html_content = render_to_string("emails/test_email.html")
+    email = EmailMultiAlternatives(subject=subject,body=text_content,from_email=settings.DEFAULT_FROM_EMAIL, to=[recipient_email],)
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    
+    
