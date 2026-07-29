5c4740eeb4ec9c4385dbbf258e9687139d18e08a
amor <amorsaralynn@gmail.com>
2026-07-29 10:44:26 +0300

    create an Email view

--- ARGUS_app/urls.py
+++ ARGUS_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path , include
-from .views import SensorReadingView,AlertViewSet
+from .views import SensorReadingView,AlertViewSet,TestEmailView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView , TokenRefreshView)
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("token/",TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
 
 ]
 
--- ARGUS_app/views.py
+++ ARGUS_app/views.py
@@ -4,8 +4,10 @@ from .models import SensorReading , Alert
 from .serializers import SensorReadingSerializer
 from .serializers import AlertSerializer
 from .permissions import CanManageAlerts , CanViewSensorReadings
-
-
+from .utils import send_test_email
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -32,3 +34,10 @@ class AlertViewSet(
     queryset = Alert.objects.all()
     serializer_class = AlertSerializer
     permission_classes = [CanManageAlerts]
+
+class TestEmailView(APIView):
+    def post(self,request):
+        send_test_email(request.user.email)
+
+        return Response({"message":"Test email sent successfully"},status=status.HTTP_200_OK)
+                      
