8a7a9cb1c1dd67e43d37c0e891aad21f045cfe09
amor <amorsaralynn@gmail.com>
2026-07-30 20:34:13 +0300

    add validation in change password serializer

--- ARGUS_app/serializers.py
+++ ARGUS_app/serializers.py
@@ -49,3 +49,11 @@ class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
     confirm_password = serializers.CharField(required=True)
+
+    def validate(self,attrs):
+        if attrs["new_password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {"confirm_password":"Passwords do not match."}
+           )
+
+        return attrs
