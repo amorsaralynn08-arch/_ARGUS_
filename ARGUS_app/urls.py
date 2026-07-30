1e774d9bbd954cdda78be72bc10e9de8afb9297f
amor <amorsaralynn@gmail.com>
2026-07-30 19:48:21 +0300

    add password reset urls

--- ARGUS_app/urls.py
+++ ARGUS_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path , include
 from .views import SensorReadingView,AlertViewSet,TestEmailView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView , TokenRefreshView)
+from django.contrib.auth.views import (PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView)
 
 
 router = DefaultRouter()
@@ -15,6 +16,12 @@ urlpatterns = [
     path("token/",TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("test-email/", TestEmailView.as_view(), name="test-email"),
+    path("password-reset/",PasswordResetView.as_view(),name="password-reset"),
+    path("password-reset/done/",PasswordResetDoneView.as_view(),name="password_reset_done",),
+    path("reset/<uidb64>/<token>/",PasswordResetConfirmView.as_view(),name="password_reset_confirm",),
+    path("reset/done/",PasswordResetCompleteView.as_view(),name="password_reset_complete",),
+
+
 
 ]
 
