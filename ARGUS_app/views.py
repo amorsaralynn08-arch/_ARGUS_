54835b30d4e09fd04430ec4d821661b73b372b0e
amor <amorsaralynn@gmail.com>
2026-07-30 21:30:04 +0300

    fix indentation

--- ARGUS_app/views.py
+++ ARGUS_app/views.py
@@ -50,14 +50,18 @@ class ChangePasswordView(APIView):
         if serializer.is_valid():
             user = request.user
 
-        if not user.check_password(
+            if not user.check_password(
             serializer.validated_data["old_password"]
         ):
-            return Response({
+              return Response({
                 "old_password":"Incorrect password."},
                 status=status.HTTP_400_BAD_REQUEST,)
 
-        user.set_password(serializer.validated_data["new_password"])
-        user.save()
-        return Response({"message":"Password changed successfully."},status=status.HTTP_200_OK,)
-    
+            user.set_password(serializer.validated_data["new_password"])
+            user.save()
+            return Response({"message":"Password changed successfully."},status=status.HTTP_200_OK,)
+       
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
