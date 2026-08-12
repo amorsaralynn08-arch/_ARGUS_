9138398ebb3efe04391e9c7956969e0f4ef3d154
amor <amorsaralynn@gmail.com>
2026-08-12 14:13:48 +0300

    connect auth provider to main.jsx and wire app.jsx

--- Argus-frontend/src/App.jsx
+++ Argus-frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
-  
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="*"
+          element={<Navigate to="/register" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
